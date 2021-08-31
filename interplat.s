ba00aa95968bf1cd545a0e3fdd63c3ea7c9d3250
Jonathan Fuerth <jfuerth@gmail.com>
2021-08-30 22:44:47 -0400

    Implement mob struct with 16bit x,y,dx,dy

--- interplat.s
+++ interplat.s
@@ -8,48 +8,33 @@ spriteimg=screen+1024-8
 ; temporary vars and pointers
 r0=$f7
 r1=$f8
-ptr0=$f9
-ptr0h=$fa
+r2=$f9
+r3=$fa
+ptr0=$fb
+ptr0h=$fc
 
-
-init
-	; sprite 0
-	lda #100
-	sta $d000 ; spr0 x
-	lda #catfox_stand_0
-	sta spriteimg+0
-
-install
-	sei
-	lda #<handleint
-	sta $0314
-	lda #>handleint
-	sta $0315
-	cli
-	rts
+	jmp init
 
 handleint
 	.block
 	inc $d020
 
-	lda #1
-	sta $d015 ; enable spr0
+	; ack vic interrupt
+	lda #$ff
+	sta $d019
 
-	lda $d001 ; y coord
-	sec
-	sbc #29
-	lsr a
-	lsr a
-	lsr a
-	tay
-	
-	lda $d000
-	sec
-	sbc #24
-	lsr a
-	lsr a
-	lsr a
+	lda #<catmob1
+	sta ptr0
+	lda #>catmob1
+	sta ptr0+1
+
+	ldy #mobxh
+	lda (ptr0),y
 	tax
+
+	ldy #mobyh
+	lda (ptr0),y
+	tay
 
 	jsr getsc
 
@@ -59,50 +44,67 @@ handleint
 	ldx standcount
 	beq walk
 
+
+; --------- actions ------------
 stand
 	dec standcount
         lda #catfox_stand_0
-        sta spriteimg+0
+        sta catmob1+mobimg
+
+	lda #0
+	sta catmob1+mobdxl
+	sta catmob1+mobdxh
+	sta catmob1+mobdyl
+	sta catmob1+mobdyh
 
 	jmp done
 
 fall
 	inc $d001
         lda #catfox_fall_0
-        sta spriteimg+0
+        sta catmob1+mobimg
 
 	lda #15
 	sta standcount
 
+	lda #$30
+	sta catmob1+mobdyl
+	lda #0
+	sta catmob1+mobdyh
+
 	jmp done
 
-walk
-	lda walkframe
+walk	; TODO handle anim in mobupdate
+	lda catmob1+mobanimframe
 	tax
 	lsr a
 	lsr a
 	tay
 	lda walkframes,y
-	sta spriteimg+0
+	sta catmob1+mobimg
 	inx
 	cpx #(4*4) ; num of walkframes
 	bne walkframesave
 	ldx #0
 walkframesave
-	stx walkframe
+	stx catmob1+mobanimframe
 
-	inc $d000 ; TODO high bit
+	; set walk speed
+	lda #$20
+	sta catmob1+mobdxl
+	lda #0
+	sta catmob1+mobdxh
 
 	jmp done
 
 done
+	jsr mobupdate
+
 	dec $d020
 	jmp $ea31
 
 standcount
 	.byte 5
-walkframe
-	.byte 2
 walkframes
 	.byte catfox_run_0
 	.byte catfox_run_1
@@ -143,7 +145,39 @@ line    .var line+40
         .endif
         .bend
 
-; ------------- Movable Objects -----
+; ------- one time setup --------
+init
+	; catfox mob setup
+	lda #20
+	sta catmob1+mobxh
+	sta catmob1+mobyh
+	lda #catfox_stand_0
+	sta catmob1+mobimg
+
+install
+	sei
+	lda #<handleint
+	sta $0314
+	lda #>handleint
+	sta $0315
+
+	lda #$7f
+	sta $dc0d ; set to clear timer
+	lda $dc0d ; ack pending intrpt
+
+	lda #1
+	sta $d01a ; enable raster irq
+	
+	lda #140
+	sta $d012 ; set raster line num
+
+	lda #$1b
+	sta $d011 ; clear 9th bit
+
+	cli
+	rts
+
+; --------- mob structs ---------
 
 ; mob struct
 mobxl=0
@@ -155,12 +189,40 @@ mobyh=5
 mobdyl=6
 mobdyh=7
 mobcolr=8
-mobstructsz=9
+mobimg=9
+mobanimframe=10
+mobstructsz=11
+
+; --- catmob1
+catmob1
+	.repeat mobstructsz,$00
+
+; --- mob routine state ---
+spritenum
+	.byte 0
+
+; -------- mob routines --------
 
 mobupdate
 ; shift a mob into the vic registers
-; in: ptr0 - mob struct pointer
 	.block
+	lda #0
+	sta spritenum
+	sta $d010 ; sprite hi x bits
+	sta $d015 ; sprite enable
+	lda #<catmob1
+	sta ptr0
+	lda #>catmob1
+	sta ptr0+1
+	jsr updateone
+	
+	rts
+
+updateone
+; in: ptr0 - mob struct pointer
+;     spritenum - vic sprite number
+;     (must be in order staring with 0)
+
 	; apply dx
 	clc
 	ldy #mobdxl
@@ -168,23 +230,41 @@ mobupdate
 	ldy #mobxl
 	adc (ptr0),y
 	sta (ptr0),y
+	sta r0
 	ldy #mobdxh
 	lda (ptr0),y
 	ldy #mobxh
 	adc (ptr0),y
 	sta (ptr0),y
-
+	sta r1
+	
 	; apply dy
+	clc
+	ldy #mobdyl
+	lda (ptr0),y
+	ldy #mobyl
+	adc (ptr0),y
+	sta (ptr0),y
+	sta r2
+	ldy #mobdyh
+	lda (ptr0),y
+	ldy #mobyh
+	adc (ptr0),y
+	sta (ptr0),y
+	sta r3
 
-	; transfer regs to vic
+	; TODO wrap x at 40, y at 25
 
-	; store x in sprite reg
+	; calc pixel positions
+
+	; calc x pixel pos -> r1
 	; 16-bit shift 3 places left
-	ldy #mobxl
-	lda (ptr0),y
-	sta r0
-	ldy #mobxh
-	lda (ptr0),y
+
+	; merge in x-offset of 24
+	; into high byte (24<<3 = 3)
+	clc
+	lda r1
+	adc #3
 	sta r1
 
 	clc
@@ -195,15 +275,54 @@ mobupdate
 	rol r0
 	rol r1
 
-	; overflow to msb
+	; x overflow to msb
 	rol $d010
 
+	; calc y pixel pos -> r3
+	lda r2
+	clc
+	adc #$a0 ;hi bits y offset 29>>3
+	sta r2
+	lda r3
+	adc #$03 ; y offset 29>>3
+	sta r3
+
+	clc
+	rol r2
+	rol r3
+	rol r2
+	rol r3
+	rol r2
+	rol r3
+
+	; enable this sprite
+	sec
+	rol $d015
+
+	; set colour
+	ldy #mobcolr
+	lda (ptr0),y
+	ldy spritenum
+	sta $d027,y
+
+	; set animation frame
+	; TODO animate
+	ldy #mobimg
+	lda (ptr0),y
+	ldy spritenum
+	sta spriteimg,y
+
 	; pixel position to x&y regs
-	ldx r1
 	lda spritenum
-	asl a
+	asl a ;for interleaved xy coords
 	tay
-	stx $d000,y
+
+	lda r1 ; x coord
+	sta $d000,y
+
+	lda r3 ; y coord
+	sta $d001,y
 
 	rts
+
 	.bend
